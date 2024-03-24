dc483c42e1361d94b21e82704a8f224516087c35
KseniaPon <159115820+KseniaPon@users.noreply.github.com>
2024-03-24 19:33:31 +0100

    Add files via upload

---
+++ src/components/Option/index.jsx
@@ -0,0 +1,11 @@
+import { Icon } from '../Icon/index';
+import './style.css';
+
+export const Option = ({type, text, onSelected}) => {
+  return (
+    <div onClick={() => {console.log(text); onSelected(type)}} className="option">
+      <Icon type={type} />
+      <div>{text}</div>
+    </div>
+  );
+};
